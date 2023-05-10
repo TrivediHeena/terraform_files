c9fa446b78f915a052095f5baa1183865be0a8f1
trivediheena <hntrivedi72@gmail.com>
2023-05-10 17:50:26 +0530

    Terraform Create EC2 Instance

---
+++ providers.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.ACCESS_KEY
+  secret_key = var.SECRET_KEY
+}
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  type = string
+}
+variable "instance_type" {
+  type = string
+}
+variable "ACCESS_KEY" {
+  type = string
+}
+
+variable "SECRET_KEY" {
+  type = string
+}
